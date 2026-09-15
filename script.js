86165328ef824ff387fb141c1d412e4d2d9ea1f7
Axtreen <110425325+Axtreen@users.noreply.github.com>
2026-09-15 03:35:08 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("Axtreen's portfolio is alive.");
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  corePlugins: { preflight: true },
+  theme: { extend: {} },
+  plugins: [],
+};
